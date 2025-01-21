eac61577fcbb1d0c8c809c5bad70496bccababb9
4twentydev <4twenty.dev@gmail.com>
2025-01-20 18:39:53 -0700

    starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-500">helllo</h1>
+      <div className="min-h-screen flex justify-center items-center bg-gray-700 text-gray-200">
+        <h1 className="text-red-500 text-3xl">4twentydev</h1>
+      </div>
     </>
   );
 }
